2e6edf166daaf899d009892831607b339dac2ffa
chingweichan <willy756.taiwan@gmail.com>
2025-01-04 17:01:12 +0800

    feat : 更改成繁體中文

--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -1,23 +1,23 @@
 export default {
   all: '全部',
-  search: '搜索',
-  readmeTip: '点击左侧存储库查看',
-  loginTip: 'GitHub APP 授权登录',
-  userTitle: '{username} 的星标存储库',
+  search: '搜尋',
+  readmeTip: '點擊左側儲存庫查看',
+  loginTip: 'GitHub APP 授權登入',
+  userTitle: '{username} 的星標儲存庫',
   sort: {
-    ascend: '升序',
-    descend: '降序',
+    ascend: '由小到大排序',
+    descend: '由大到小排序',
   },
   category: {
-    star: '你的星标',
+    star: '你的星標',
     ranking: 'Github 排行榜',
   },
   repo: {
-    filterTip: '开发者 {div} 仓库名 {div} 描述',
+    filterTip: '開發者 {div} 儲存庫名稱 {div} 描述',
     sort: {
-      star: '按星标排序',
-      time: '按时间排序',
+      star: '按星標排序',
+      time: '按時間排序',
     },
-    updating: '存储库更新中',
+    updating: '儲存庫更新中',
   },
 };
